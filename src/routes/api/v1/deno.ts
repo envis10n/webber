15012a810ac039f9241227b6b70d7f7660271974
Envis10n <envis10n@protonmail.com>
2020-08-10 02:39:12 -0500

    Again

--- src/routes/api/v1/deno.ts
+++ src/routes/api/v1/deno.ts
@@ -42,15 +42,19 @@ export async function denoEval(lang: "ts" | "js", code: string, id: string): Pro
                 let hadError = false;
                 if (err) {
                     hadError = true;
-                    if (err.killed) {
-                        output = "error: Script timed out.";
-                    } else if (stderr.trim().length > 0) {
-                        const s1 = stripAnsi(stderr.trim()).split("\n").slice(1);
-                        let s2 = s1[0];
-                        if (s1.length > 2) s2 = s1.join("\n");
-                        output = s2.replace(new RegExp(fPath.replace(/\\/g, "/"), 'g'), `_${id}.${fExt}`);
-                    } else {
-                        output = err.message;
+                    try {
+                        if (err.killed) {
+                            output = "error: Script timed out.";
+                        } else if (stderr.trim().length > 0) {
+                            const s1 = stripAnsi(stderr.trim()).split("\n").slice(1);
+                            let s2 = s1[0];
+                            if (s1.length > 2) s2 = s1.join("\n");
+                            output = s2.replace(new RegExp(fPath.replace(/\\/g, "/"), 'g'), `_${id}.${fExt}`);
+                        } else {
+                            output = err.message;
+                        }
+                    } catch(e) {
+                        reject(err.message);
                     }
                 } else {
                     output = stripAnsi(stdout.trim());
