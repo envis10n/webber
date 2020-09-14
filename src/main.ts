79619f61d1a4a9584b9170ee054340e4ee271c8c
Envis10n <envis10n@protonmail.com>
2020-09-13 19:40:06 -0500

    whoops.

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ import path from "path";
 (async (): Promise<void> => {
     await Routes(App);
     App.use(serve(path.join(process.cwd(), "public")));
-    App.use(async (ctx, next) => {
-        ctx.set("Access-Control-Allow-Origin", "*");
-        ctx.set("Access-Control-Allow-Methods", "POST, GET");
-        ctx.set("Access-Control-Max-Age", "86400");
-        await next();
-    });
     App.listen(3056, () => {
         console.log("Koa Webserver Online.");
     });
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -5,7 +5,12 @@ const Routes: Router = new Router({
     prefix: "/api",
 });
 
-export default async function(app: Router): Promise<void> {
+export default async function (app: Router): Promise<void> {
     await loadRoutes(Routes, __dirname);
+    app.options("*", async (ctx, next) => {
+        ctx.set("Access-Control-Allow-Origin", "*");
+        ctx.set("Access-Control-Allow-Methods", "POST, GET");
+        ctx.set("Access-Control-Max-Age", "86400");
+    });
     app.use(Routes.routes());
 }
