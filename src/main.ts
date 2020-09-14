54fd4e15596f30b1025ec8530a77f2038a134c70
Envis10n <envis10n@protonmail.com>
2020-09-13 19:36:01 -0500

    Add headers to fix CORS preflight

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import path from "path";
     App.use(serve(path.join(process.cwd(), "public")));
     App.use(async (ctx, next) => {
         ctx.set("Access-Control-Allow-Origin", "*");
+        ctx.set("Access-Control-Allow-Methods", "POST, GET");
+        ctx.set("Access-Control-Max-Age", "86400");
         await next();
     });
     App.listen(3056, () => {
